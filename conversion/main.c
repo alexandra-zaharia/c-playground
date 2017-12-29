47bca727623c890312ae1ae26cba5305fc94445a
Alexandra Zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2017-12-29 09:04:59 +0100

    Restrict scope

--- conversion/main.c
+++ conversion/main.c
@@ -4,24 +4,23 @@
 #include <stdlib.h>
 
 int main() {
-    int temp_scale;    /* temperature scale to convert from */
-    int conv_scale;    /* temperature scale to convert to */
-    
     double init_temp;  /* temperature to convert */
-    double conv_temp;  /* temperature after conversion */
 
     for (;;) {
-        temp_scale = get_temp_scale();
+        /* temperature scale to convert from */
+        int temp_scale = get_temp_scale(); 
         
         printf("\nEnter temperature to convert : ");
         scanf("%lf",&init_temp);
 
-        conv_scale = get_conv_scale(temp_scale);
+        /* temperature scale to convert to */
+        int conv_scale = get_conv_scale(temp_scale);
 
-        conv_temp = do_conversion(temp_scale, conv_scale, init_temp);
+        /* temperature after conversion */
+        double conv_temp = do_conversion(temp_scale, conv_scale, init_temp);
         
         display_result(init_temp, temp_scale, conv_temp, conv_scale);
     }
 
-    return EXIT_FAILURE;  /* should never be reached */
+    return EXIT_FAILURE; /* should never be reached */
 }
